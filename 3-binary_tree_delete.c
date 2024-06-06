cfd38ed23a38b1fd09c401c6abf9f7eb375b2c8c
Macaulay Israel <israelmacaulay8@gmail.com>
2024-06-06 05:12:12 -0700

    0x03 tk 03 vi 01

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,12 +3,10 @@
 #include <stdio.h>
 
 /**
- * binary_tree_insert_left - create a binary node.
+ * binary_tree_delete - create a binary node.
  * Description: A function that creates a new node of type
  *              binary tree and attach it to a parent node.
- *
- * @parent: a pointer to a binatr_tree node that serve as a parent.
- * @value: the value to be inputed into the created node.
+ * @tree: a pointer to a binatr_tree node that serve as a parent.
  *
  * Return: a pointer of type binary_tree on success or null.
  */
@@ -17,18 +15,13 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 
 	if (tree->left == NULL && tree->right == NULL)
-    {
-        free(tree)
+	{
+		free(tree);
 		return (NULL);
-    }
-
-    if (tree->left != NULL)
-    {
-        binary_tree_delete(tree->left)
-    }
-    if (tree->right == NULL)
-    {
-        binary_tree_delete(tree->right)
-    }
+	}
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left)
+	if (tree->right == NULL)
+		binary_tree_delete(tree->right)
 	return
 }
