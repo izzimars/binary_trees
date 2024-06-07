f7a06b844f1e303d9555bb9bdcbf6a7a28aca206
izzimars <israelmacaulay8@gmail.com>
2024-06-07 08:03:55 -0700

    0x00 tk-06 vi-00

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_preorder - create a binary node.
+ * Description: A function that creates a new node of type
+ *              binary tree and attach it to a parent node.
+ * @tree: a pointer to a binatr_tree node that serve as a parent.
+ * @func: a pointer to a binatr_tree node that serve as a parent.
+ *
+ * Return: a pointer of type binary_tree on success or null.
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NUll)
+        return (void)
+    func(tree->n)
+    binary_tree_preorder(tree->left);
+    binary_tree_preorder(tree->right);
+}
